ba0cec8d69674c88daf74454cbdd2ef3dd812206
LiuXiaolong <liuxiaolong19920720@gmail.com>
2016-10-14 23:08:47 +0800

    Delete defined resize() function
    Delete defined resize() function and replace it by cv2.resize().

--- classify_nsfw.py
+++ classify_nsfw.py
@@ -15,30 +15,6 @@ import time
 import caffe
 import cv2
 
-'''
-def resize_image(data, sz=(256, 256)):
-    """
-    Resize image. Please use this resize logic for best results instead of the 
-    caffe, since it was used to generate training dataset 
-    :param str data:
-        The image data
-    :param sz tuple:
-        The resized image dimensions
-    :returns bytearray:
-        A byte array with the resized image
-    """
-    #img_data = str(data)
-    #im = Image.open(StringIO(data))
-    im0 = cv2.imread(data,1)
-    im = 
-    if im.mode != "RGB":
-        im = im.convert('RGB')
-    imr = im.resize(sz, resample=Image.BILINEAR)
-    fh_im = StringIO()
-    imr.save(fh_im, format='JPEG')
-    fh_im.seek(0)
-    return bytearray(fh_im.read())
-'''
 def caffe_preprocess_and_compute(pimg, caffe_transformer=None, caffe_net=None,
     output_layers=None):
     """
